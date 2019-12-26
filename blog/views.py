b016204ecb62f8afad8a79e0304e4319551f86a9
Shouvik Mandal <shouvik009@yahoo.com>
2019-12-27 01:48:55 +0530

    Pagination added with function based view

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, \
+                                  PageNotAnInteger
+
 
 def post_list(request):
-    posts = Post.published.all()
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3) # 3 posts in each page
+    page = request.GET.get('page')
+    
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # if page is not an integer then deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # if page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
     return render(request,
-                 'blog/post/list.html',
-                 {'posts': posts})
+                'blog/post/list.html',
+                {'page': page,
+                'posts': posts})
 
 def post_detail(request, year, month, day, post):
     try:
@@ -23,3 +38,5 @@ def post_detail(request, year, month, day, post):
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
+
+
